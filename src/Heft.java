bd361307c08a7b85e3982ce605e20d0fe9f660e4
misterpigeon <steven.zelin.li@gmail.com>
2017-01-07 09:51:10 -0500

    Working on fixing the png white background issue. Going to implement some quality of life fixes

--- src/Heft.java
+++ src/Heft.java
@@ -13,6 +13,7 @@ import java.util.BitSet;
 public class Heft extends Canvas {
     public int page; // This handles the KeyPress event to determine what a key does on a page;
     public PhysEntity slime0;
+    public PhysEntity slime1;
     boolean repaintInProgress = false;
     protected HeftKeyListener listener;
 
@@ -26,8 +27,14 @@ public class Heft extends Canvas {
 
         // We can set the graphics reference to null until I generate the object, alternatively I can also set the reference, it doesn't really matter
         slime0 = new PhysEntity("Slime","C:\\Users\\steve\\OneDrive\\Documents\\Java\\Roguelike\\Sprites\\slime_ss.png",
-                68, 2, 70, 135, 10, true, 2, 550, 450, 1100, 900, 0, true); // has only a death animation
-        slime0.startAI();
+        68, 2, 70, 135, 10, true, 1, 550, 450,
+                1100, 900, 0, true, 100, 4); // has only a death animation
+        //slime0.startAI();
+
+        slime1 = new PhysEntity("Slime","C:\\Users\\steve\\OneDrive\\Documents\\Java\\Roguelike\\Sprites\\slime_ss.png",
+                68, 2, 70, 135, 10, true, 1, 750, 450,
+                1100, 900, 0, true, 100, 4); // has only a death animation
+        //slime1.startAI();
         Chrono chrono = new Chrono(this);
         new Timer(10, chrono).start();
     }
@@ -37,7 +44,9 @@ public class Heft extends Canvas {
 
         @Override
         public void keyTyped(KeyEvent e) {
-            // dont care
+            if(e.getKeyChar() == 'f' || e.getKeyChar() == 'F'){
+                slime0.damage(1, 0);
+            }
         }
 
         @Override
@@ -63,28 +72,53 @@ public class Heft extends Canvas {
 
         repaintInProgress = true;
 
-        int indicator = 0;
+        if(slime0 != null) {
+            int indicator = 0;
+            if (listener.isKeyPressed('w') || listener.isKeyPressed('W')) {
+                slime0.moveUp();
+                indicator++;
+            }
+            if (listener.isKeyPressed('a') || listener.isKeyPressed('A')) {
+                slime0.moveLeft();
+                indicator++;
+            }
+            if (listener.isKeyPressed('s') || listener.isKeyPressed('S')) {
+                slime0.moveDown();
+                indicator++;
+            }
+            if (listener.isKeyPressed('d') || listener.isKeyPressed('D')) {
+                slime0.moveRight();
+                indicator++;
+            }
+            if (indicator == 0 && slime0.getAIStatus() == false) {
+                slime0.setNeutral();
+            }
 
-        if(listener.isKeyPressed('w') || listener.isKeyPressed('W')){
-            slime0.moveUp();
-            indicator++;
-        }
-        if(listener.isKeyPressed('a') || listener.isKeyPressed('A')){
-            slime0.moveLeft();
-            indicator++;
-        }
-        if(listener.isKeyPressed('s') || listener.isKeyPressed('S')){
-            slime0.moveDown();
-            indicator++;
-        }
-        if(listener.isKeyPressed('d') || listener.isKeyPressed('D')){
-            slime0.moveRight();
-            indicator++;
-        }
-        if(indicator == 0){
-            slime0.setNeutral();
         }
 
+        if(slime1 != null) {
+            int indicator = 0;
+            if (listener.isKeyPressed('i') || listener.isKeyPressed('I')) {
+                slime1.moveUp();
+                indicator++;
+            }
+            if (listener.isKeyPressed('j') || listener.isKeyPressed('J')) {
+                slime1.moveLeft();
+                indicator++;
+            }
+            if (listener.isKeyPressed('k') || listener.isKeyPressed('K')) {
+                slime1.moveDown();
+                indicator++;
+            }
+            if (listener.isKeyPressed('l') || listener.isKeyPressed('L')) {
+                slime1.moveRight();
+                indicator++;
+            }
+            if (indicator == 0 && slime1.getAIStatus() == false) {
+                slime1.setNeutral();
+            }
+
+        }
         Dimension size = getSize(); // get size of canvas
 
         BufferStrategy strategy = getBufferStrategy();
@@ -93,7 +127,15 @@ public class Heft extends Canvas {
         graphics.setColor(Color.WHITE);
         graphics.fillRect(0, 0, size.width, size.height); // refreshing the screen
 
-        slime0.generate(graphics);
+        if(slime0 != null) {
+            if (!slime0.isDead()) slime0.generate(graphics);
+            else slime0 = null;
+        }
+
+        if(slime1 != null) {
+            if (!slime1.isDead()) slime1.generate(graphics);
+            else slime1 = null;
+        }
 
         if(graphics != null) graphics.dispose();
 
@@ -102,7 +144,6 @@ public class Heft extends Canvas {
         Toolkit.getDefaultToolkit().sync(); // sync blitter page
 
         repaintInProgress = false;
-
     }
 }
 
